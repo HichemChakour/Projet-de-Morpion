32f70616f7f49ebb3d966cd317bc879099baa3d1
Hichem Chakour <81690489+HichemChakour@users.noreply.github.com>
2024-03-13 13:34:32 +0100

    ajout base projet

---
+++ morpion/src/main/java/com/morpion/morpion/HelloController.java
@@ -0,0 +1,22 @@
+package com.morpion.morpion;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.animation.FadeTransition;
+import javafx.util.Duration;
+
+public class HelloController {
+    public Button button1;
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    private void onHelloButtonClick() {
+        FadeTransition transition = new FadeTransition(Duration.seconds(1),button1);
+        transition.setFromValue(1);
+        transition.setToValue(0);
+        transition.play();
+    }
+
+}
