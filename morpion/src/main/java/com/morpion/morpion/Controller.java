e7794e4a8ccd46afa0d094106f9dc90586e5f77e
hchakour2003 <hchakour2003@gmail.com>
2024-03-13 14:33:03 +0100

    ajout de la fonction (vide) Setting IA

--- morpion/src/main/java/com/morpion/morpion/Controller.java
+++ morpion/src/main/java/com/morpion/morpion/Controller.java
@@ -7,16 +7,12 @@ import javafx.animation.FadeTransition;
 import javafx.util.Duration;
 
 public class Controller {
-    public Button button1;
     @FXML
-    private Label welcomeText;
+    private Button MenuSettingIA;
 
     @FXML
-    private void onHelloButtonClick() {
-        FadeTransition transition = new FadeTransition(Duration.seconds(1),button1);
-        transition.setFromValue(1);
-        transition.setToValue(0);
-        transition.play();
+    private void SettingIA() {
+
     }
 
 }
